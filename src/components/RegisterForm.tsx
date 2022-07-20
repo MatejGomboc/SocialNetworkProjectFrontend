a01160c0e24c2522612f063a6cec3cddc026bbe7
Matej Gomboc <matejg03@gmail.com>
2022-07-20 19:09:57 +0200

    Update.

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import LinkButton from "./LinkButton";
 import "./RegisterForm.scss";
 
 const RegisterForm: React.FC = (): JSX.Element => {
     return(
         <div className="RegisterForm">
+            <h1 className="RegisterForm">Create new account</h1>
+            <input
+                type="text"
+                className="RegisterForm"
+                placeholder="username"
+            />
+            <input
+                type="password"
+                className="RegisterForm"
+                placeholder="password"
+            />
+            <input
+                type="password"
+                className="RegisterForm"
+                placeholder="repeat password"
+            />
+            <input
+                type="email"
+                className="RegisterForm"
+                placeholder="email address"
+            />
+            <LinkButton text="BACK" link="/"/>
         </div>
     );
 }
